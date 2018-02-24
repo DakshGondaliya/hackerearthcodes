3e94015f415ed0db61f3e5bdfba13e6a75536a72
DakshGondaliya <gondaliyadaksh@gmail.com>
2018-02-24 23:16:12 +0530

    Create Life,TheUniverseAndEverything.cpp

---
+++ Life,TheUniverseAndEverything.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    while(1)
+    {
+      cin >> n;
+      if(n==42)
+      {
+          break;
+      }
+      else
+      {
+          cout << n << "\n";
+      }
+    }
+    return 0;
+}
